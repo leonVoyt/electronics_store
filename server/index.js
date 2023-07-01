e44e0908f1d457237f700add6085c6a5bfa0eff4
Leon <6860571@stud.nau.edu.ua>
2023-07-01 17:59:32 +0300

    sync with server

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ app.get('/', (req, res) => {
 const start = async () => {
   try {
     await sequelize.authenticate()
-    await sequelize.sync({ force: true })
+    await sequelize.sync()
     app.listen(PORT, () => {
       console.log(`server start: http://localhost:${PORT}`)
     })
